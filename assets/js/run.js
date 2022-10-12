dea67bd71fa78dbdeed7a00ba74c03f15862251d
tibo.krekelbergh <tibo.krekelbergh@student.howest.be>
2022-10-12 10:34:27 +0200

    Added redirect to https

---
+++ assets/js/run.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init()
+{
+    /* ALS JE LIVE SERVER WILT GEBRUIKEN ZET DIT IN COMMENTS */
+    if (location.protocol !== 'https:') {
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+}
