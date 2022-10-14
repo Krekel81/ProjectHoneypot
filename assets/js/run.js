0400ae11126fb699e67e5b581d9d5779273807d7
tibo.krekelbergh <tibo.krekelbergh@student.howest.be>
2022-10-14 21:01:54 +0200

    fixed http to https in js

--- assets/js/run.js
+++ assets/js/run.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init()
 {
-    /* ALS JE LIVE SERVER WILT GEBRUIKEN ZET DIT IN COMMENTS 
+    /* ALS JE LIVE SERVER WILT GEBRUIKEN ZET DIT IN COMMENTS */
     if (location.protocol !== 'https:') {
         location.replace(`https:${location.href.substring(location.protocol.length)}`);
     }
-    */
+    
 }
