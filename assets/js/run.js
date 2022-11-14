d2470200dd868754dd1c95d27d933c9fca1b55f4
tibo.krekelbergh <tibo.krekelbergh@student.howest.be>
2022-11-14 14:46:44 +0100

    few edits

--- assets/js/run.js
+++ assets/js/run.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', init);
 function init()
 {
     /* ALS JE LIVE SERVER WILT GEBRUIKEN ZET DIT IN COMMENTS */
+/*
     if (location.protocol !== 'https:') {
         location.replace(`https:${location.href.substring(location.protocol.length)}`);
     }
-    
+*/
 }
+
