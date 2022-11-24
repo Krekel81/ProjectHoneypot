3d7cc8e55f66abb4ea8b5bfe90a29ec84c39f77f
tibo.krekelbergh <tibo.krekelbergh@student.howest.be>
2022-11-24 15:56:17 +0100

    If file is too big, show message

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Image;
+use Intervention\Image\Size;
 
 class ImageController extends Controller
 {
@@ -14,20 +16,27 @@ class ImageController extends Controller
         session_start();
         if($request->hasFile('avatar'))
         {
-            $user = User::where("name", $_SESSION["username"])->first();
+            try
+            {
+                if(getimagesize($request->file('avatar')) > 2000) return view('fileTooBig');
+                $user = User::where("name", $_SESSION["username"])->first();
 
-            $avatar = $request->file('avatar');
-            $filename = time().'.'.$avatar->getClientOriginalExtension();
+                $avatar = $request->file('avatar');
 
-            Image::make($avatar)->resize(300, 300)->save(public_path('/uploads/avatars/'.$user->name.'/'.$filename));
+                $filename = time().'.'.$avatar->getClientOriginalExtension();
 
-            //$avatar->move(public_path('/uploads/avatars/'.$user->name.'/'), $filename);
-
-            $user->avatar = $filename;
-            $user->save();
+                Image::make($avatar)->resize(300, 300)->save(public_path('/uploads/avatars/'.$user->name.'/'.$filename));
 
 
-            return view('landing', array('user' => $user));
+                $user->avatar = $filename;
+                $user->save();
+
+
+                return view('landing', array('user' => $user));
+            }
+            catch (Exception $e) {
+                echo 'Caught exception: ',  $e->getMessage(), "\n";
+            }
         }
     }
 }
---
+++ resources/views/filetoobig.blade.php
@@ -0,0 +1,45 @@
+<?php
+if(isset($_SESSION['loggedIn']))
+{
+    if(!($_SESSION["loggedIn"]))
+    {
+        header("Location: /");
+        exit();
+    }
+}
+else
+{
+    $_SESSION['loggedIn'] = false;
+    header("Location: /");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Honeypot - Group 3</title>
+    <!-- Our Css -->
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/screen.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://fonts.cdnfonts.com/css/matrix" rel="stylesheet">
+    <!-- Our Scripts -->
+    <script src="assets/js/run.js"></script>
+    <script src="assets/js/uploadedfile.js"></script>
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<canvas id="canv" width="500" height="200"></canvas>
+<!-- partial -->
+
+<script src="assets/js/script.js"></script>
+
+<main>
+    <div id="landings">
+
+    </div>
+</main>
+</body>
+</html>
