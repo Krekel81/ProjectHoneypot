77a78990246fe82765b8cbb9214b9d106ca6025a
tibo.krekelbergh <tibo.krekelbergh@student.howest.be>
2022-11-24 17:02:00 +0100

    in comments

--- public/assets/js/checkIfLoggedOut.js
+++ public/assets/js/checkIfLoggedOut.js
@@ -4,10 +4,5 @@ document.addEventListener("DOMContentLoaded", checkIfUserHasBeenLoggedOut);
 
 function checkIfUserHasBeenLoggedOut()
 {
-    window.onbeforeunload = function(event) {
-        fetch('api/users/logOff', {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'}
-        });
-     };
+    console.log("In Development");
 }
