308ee0443984e9c69406a14bafa1019ba5ec75ab
tibo.krekelbergh <tibo.krekelbergh@student.howest.be>
2022-11-25 08:58:19 +0100

    https

--- public/assets/js/run.js
+++ public/assets/js/run.js
@@ -6,11 +6,11 @@ function init()
 {
     setTimeout (console.log.bind (console, "%cStop searching for what you shall not find!", "font-weight: bold; font-size: 0.8rem;color: red; padding: 1rem;"));
     /* ALS JE LIVE SERVER WILT GEBRUIKEN ZET DIT IN COMMENTS */
-/*
+
     if (location.protocol !== 'https:' && location.protocol !== localhost ) {
         location.replace(`https:${location.href.substring(location.protocol.length)}`);
     }
-*/
+
     document.querySelectorAll("#challengeList button").forEach((element, index) => {
         const page = "challenge" + (index+1);
         element.addEventListener('click', () => redirectHTML(page));
